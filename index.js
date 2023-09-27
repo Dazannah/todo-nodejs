db1d7419a2cb81158e19f40407bd40d38743c3bb
Dazannah <beattius@gmail.com>
2023-09-27 12:28:47 +0200

    database js code moved to index js

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require("express")
 const server = express()
 
-const { startServer } = require("./database")
-const router = require("./router")
+const { MongoClient } = require("mongodb")
+const session = require("express-session")
+const MongoStore = require("connect-mongo")
 
-server.set("view engine", "ejs")
-server.set("public")
-server.use(router)
+const router = require("./router")
+const client = new MongoClient("mongodb://root:root@localhost:27017/")
 
 startServer()
-module.exports = server
+
+async function startServer() {
+  await client.connect()
+  const database = client.db("todo")
+
+  const sessionOptions = session({
+    secret: "some safely generated secret",
+    store: MongoStore.create({ client: client }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true }
+  })
+
+  server.use(sessionOptions)
+
+  server.set("view engine", "ejs")
+  server.use(express.urlencoded({ extended: false }))
+  server.set("public")
+  server.use("/", router)
+
+  module.exports = { database }
+  server.listen(3000)
+}
